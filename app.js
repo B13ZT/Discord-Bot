a69d11c356531a877ec9d7906f688a5aa7346126
solinox <arbosch@vt.edu>
2016-08-28 09:49:17 -0400

    added !joke

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ bot.on("message", function(msg) {
 	var altPrefixes = ["#"];
 	altPrefixes.forEach(function(alt) {
 		if(msg.content.startsWith(alt))
-			msg.content = "!" + msg.content.slice(1);
+			msg.content = prefix + msg.content.slice(1);
 	});
 
 	// Checks if message starts with prefix. If not, ends
 	if(!msg.content.startsWith(prefix)) return;
-	
+	else msg.content = msg.content.slice(1);
+
 	// Preset one word -> response messages
 	var responseObject = {
 		ayy: "Ayy, lmao!",
@@ -36,16 +37,16 @@ bot.on("message", function(msg) {
 		shrug: "¯\\_(ツ)_/¯",
 		tableflip: "(╯°□°）╯︵ ┻━┻",
 		unflip: "┬──┬ ノ( ゜-゜ノ)",
-		poundsign: "Let's all take a moment and reflect on the fact that Tommy and Kaitlyn are to be wed under a great big #poundsign. Never forget!"
+		poundsign: "Save the date! TomKat say their I do's on September 17th 2017!"
 	};
-	if(responseObject[msg.content.slice(1)]) {
-		bot.sendMessage(msg, responseObject[msg.content.slice(1)]);
+	if(responseObject[msg.content]) {
+		bot.sendMessage(msg, responseObject[msg.content]);
 	}
-	else if(msg.content.startsWith(prefix + "help")) {
+	else if(msg.content.startsWith("help")) {
 		var keys = Object.keys(responseObject).reduce(function(prev, curr) { return prev + "\n" + curr; }, "")
 		bot.reply(msg, "Help: Use the '!' prefix. List of quick auto-replies:" + keys + "\n!RemindMe uses the format [number] [unit] [message], enter '!RemindMe help' for more info");
 	}
-	else if(msg.content.startsWith(prefix + "RemindMe") || msg.content.startsWith(prefix + "remindme")) {
+	else if(msg.content.startsWith("RemindMe") || msg.content.startsWith("remindme")) {
 		var help = "Format: '!RemindMe [number] [units] [message]' Units can be seconds, minutes, hours, days, weeks, months, or years.\nFor shorthand, use s = seconds, mi = minutes, h = hours, d = days, m = months, y = years.";
 		var args = msg.content.split(" ");
 		if(args[1] === "help" || args[1] === "Help" || args[1] === "HELP" || args.length < 3) {
@@ -80,6 +81,20 @@ bot.on("message", function(msg) {
 		setTimeout(function(){
 			bot.reply(msg, `Reminding you about "${reminderMessage}"`);
 		}, num);
+	}
+	else if(msg.content.startsWith("joke") {
+		fetch("https://www.reddit.com/r/jokes/top.json", {method: "GET"})
+		.then(function(r) {
+			if(r.ok) Promise.resolve(r.json());
+			else Promise.reject(r.statusText);
+		})
+		.then(function (r) {
+			var num = Math.floor(Math.random() * 25);
+			r = r.data.children[num].data;
+			var joke = { title: r.title, text: r.selftext, url: r.url };
+			bot.reply(msg, joke.title + "\n\n" + joke.text + "\n\nLink: " + joke.url;
+		})
+		.catch(function(err) { console.log("Joke Error: " + err); bot.reply(msg, "This is no time for a joke. I also can't reach /r/jokes right now, but that's besides the point. Maybe try later."); });
 	}
 });
 
